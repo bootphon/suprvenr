84dfdf3cc62110a48f2fb131e53ee885ace034c1
Ewan Dunbar <emd@umd.edu>
2016-06-29 18:11:05 +0200

    added variance_proportion PCA

--- suprvenr/R/transform.R
+++ suprvenr/R/transform.R
@@ -16,12 +16,19 @@ reduce_to <- function(m, k=NULL) {
 #' Apply PCA transform
 #' @description Applies PCA and reduces to \code{k} dimensions; works by calling prcomp with z-scoring first
 #' @param m A matrix
-#' @param k Fixed number of dimensions (principal components) to return; defaults to
-#' no dimensionality reduction if \code{k} is \code{NULL} - reducing by variance explained is not supported 
+#' @param k Fixed number of dimensions (principal components) to return
+#' @param prop_var Select dimension by proportion of explained variance (need to explain
+#' at least \code{prop_var}); overrides \code{k} if set
 #' @return m decorrelated by PCA-rotation
 #' @export
-pca <- function(m, k=NULL) {
-  return(reduce_to(prcomp(m, scale=T)$x, k))
+pca <- function(m, k=NULL, prop_var=NULL) {
+  pc <- prcomp(m, scale=T)
+  if (!is.null(prop_var)) {
+    prop_var <- min(prop_var, 1.0)
+    prop_vars_all <- pcasds_variance_proportion(pc$sdev) 
+    k <- which(prop_vars_all >= prop_var)[1]
+  }
+  return(reduce_to(pc$x, k))
 }
 
 #' Apply z-score transform
@@ -38,25 +45,38 @@ zscore <- function(m, k=NULL) {
 #' Apply whitening PCA transform
 #' @description Applies PCA and reduces to \code{k} dimensions, then divides out the variance
 #' @param m A matrix
-#' @param k Fixed number of dimensions (principal components) to return; defaults to
-#' no dimensionality reduction if \code{k} is \code{NULL}
+#' @param k Fixed number of dimensions (principal components) to return
+#' @param prop_var Select dimension by proportion of explained variance (need to explain
+#' at least \code{prop_var}); overrides \code{k} if set
 #' @return m whitened by PCA-rotation and z-scoring
 #' @export
-whiten_pca <- function(m, k=NULL) {
-  return(zscore(prcomp(m, scale=T)$x, k))
+whiten_pca <- function(m, k=NULL, prop_var=NULL) {
+  pc <- prcomp(m, scale=T)
+  if (!is.null(prop_var)) {
+    prop_var <- min(prop_var, 1.0)
+    prop_vars_all <- pcasds_variance_proportion(pc$sdev) 
+    k <- which(prop_vars_all >= prop_var)[1]
+  }
+  return(zscore(pc$x, k))
 }
 
 #' Apply ZCA transform
 #' @description Performs PCA on \code{m}, divides out the variance, and then
 #' undoes the original PCA rotation
 #' @param m A matrix
-#' @param k Fixed number of dimensions (principal components) to return; defaults to
-#' no dimensionality reduction if \code{k} is \code{NULL} - reducing by variance explained is not supported 
+#' @param k Fixed number of dimensions (principal components) to return
+#' @param prop_var Select dimension by proportion of explained variance (need to explain
+#' at least \code{prop_var}); overrides \code{k} if set
 #' @return m whitened by PCA-rotation and z-scoring, and then re-rotated back to its original dimensions
 #' @export
 whiten_zca <- function(m, k=NULL) {
-  p <- prcomp(m, scale=T)
-  return(reduce_to(scale(p$x) %*% t(p$rotation), k))
+  pc <- prcomp(m, scale=T)
+  if (!is.null(prop_var)) {
+    prop_var <- min(prop_var, 1.0)
+    prop_vars_all <- pcasds_variance_proportion(pc$sdev) 
+    k <- which(prop_vars_all >= prop_var)[1]
+  }
+  return(reduce_to(scale(pc$x) %*% t(pc$rotation), k))
 }
 
 #' Center
--- suprvenr/R/transform_util.R
+++ suprvenr/R/transform_util.R
@@ -8,6 +8,14 @@ pca_variance_proportion <- function(m, k) {
   if (k > length(p$sdev)) {
     stop(paste0("k is larger than the dimension of the PCA (", length(p$sdev), ")"))
   }
-  svals <- p$sdev^2
-  return(sum(svals[1:k])/sum(svals))
+  return(pcasds_variance_proportion(p$sdev)[k])
+}
+
+#' Proportion of variance explained by set of PCA sd's
+#' @param sds A vector of sds as output by \code{\link{prcomp}}
+#' @return Sum of the singular values up to \code{k}
+#' @export 
+pcasds_variance_proportion <- function(sds) {
+  svals <- sds^2
+  return(cumsum(svals)/sum(svals))
 }
