82de4fd21f9042d74d430ff1e9ef12efa1e8b0f1
Ewan Dunbar <emd@umd.edu>
2017-01-26 16:39:43 +0100

    small fix to API for all_class_tests

--- suprvenr/R/class_test.R
+++ suprvenr/R/class_test.R
@@ -4,14 +4,14 @@ NULL
 #' Conduct class tests
 #' @description Conducts class separation tests independently on the features defined in
 #' \code{test_classes} 
-#' @param class_test_fn a function performing a classification test for a single binary
-#' feature, returning a data frame of information about the class separation, containing
-#' (at least) a column called \code{avg_loo}, giving the average of the 0-1 classification
-#' scores for all points under leave-one-out
 #' @param encoding an \code{\link{encoding}} object
 #' @param test_classes a data frame specifying the classes: it contains a column
 #' \code{fname} giving the feature  a column \code{label} giving the label of the element,
 #' and a column \code{value} giving the classification label
+#' @param class_test_fn a function performing a classification test for a single binary
+#' feature, returning a data frame of information about the class separation, containing
+#' (at least) a column called \code{avg_loo}, giving the average of the 0-1 classification
+#' scores for all points under leave-one-out
 #' @return a \code{\link{dplyr::tbl}} containing the results of applying \code{class_test_fn}
 #' for all the unique feature names in \code{test_classes$fname}; the \code{tbl} will
 #' have the following columns (at least):
@@ -20,7 +20,7 @@ NULL
 #'  \itemize{"avg_loo"}{Average leave-one-out classification score}
 #' }
 #' @export
-all_class_tests <- function(class_test_fn, encoding, test_classes) {
+all_class_tests <- function(encoding, test_classes, class_test_fn) {
   test_classes %>%
     group_by(fname) %>%
     do(class_test=class_test_fn(encoding, .)) %>%
