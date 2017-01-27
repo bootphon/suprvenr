5e0d7186a62c7a08937152814e5c5ef2c04b1a27
Ewan Dunbar <emd@umd.edu>
2017-01-27 15:14:05 +0100

    Added a generic function to do LOO given a prediction function

--- suprvenr/R/class_test.R
+++ suprvenr/R/class_test.R
@@ -1,7 +1,43 @@
 #' @importFrom dplyr %>%
+#' @importFrom foreach %dopar%
 NULL
 
-#' Conduct class tests
+#' Conduct a class test
+#' @description Conducts a class separation test on a single feature as defined in
+#' \code{test_classes_f} 
+#' @param encoding an \code{\link{encoding}} object
+#' @param test_classes a data frame specifying the two classes: it contains a column
+#' column \code{label} giving the label of the element,
+#' and a column \code{value} giving the classification label
+#' @param fit_and_predict_fn a function of three arguments: \code{x_tr}
+#' (an N by d array of training vectors), \code{y_tr} (a vector of training labels),
+#' \code{x_te} (a matrix or vector containing a point to classify), returning a
+#' predicted label.
+#' @return a \code{\link{dplyr::tbl}} containing classification scores,
+#' having at least one column,  \code{avg_loo}, giving the average
+#' leave-one-out classification score
+#' @export
+generic_test <- function(encoding, test_classes_f, fit_and_predict_fn) {
+  registerDoParallel()
+  d <- inner_join(as.tbl(encoding), test_classes_f, by="label")
+  d$y <- factor(d$value)
+  pred <- foreach(i=1:nrow(d), .combine=c) %dopar%
+    (function(j) {
+      x_tr <- d[-j,names(d) %in% encoding$fnames]
+      y_tr <- d[-j,]$y
+      x_te <- d[j,names(d) %in% encoding$fnames]
+      return(fit_and_predict_fn(x_tr, y_tr, x_te))
+    })(i)
+  correct <- pred == as.character(d$y)
+  result <- data_frame(avg_loo=mean(correct),
+                       predictions=list(data_frame(label=d$label,
+                                                   y=d$y,
+                                                   pred=pred,
+                                                   correct=correct)))
+  return(result)
+}
+
+#' Conduct class tests for several features
 #' @description Conducts class separation tests independently on the features defined in
 #' \code{test_classes} 
 #' @param encoding an \code{\link{encoding}} object
