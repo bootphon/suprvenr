78a1c13329a881a6d87ebf8573f2ddc86de653f3
Ewan Dunbar <emd@umd.edu>
2017-03-01 17:06:48 +0100

    fixes dependency in class_test

--- suprvenr/R/class_test.R
+++ suprvenr/R/class_test.R
@@ -51,8 +51,8 @@ generic_test <- function(encoding, test_classes_f, fit_and_predict_fn,
     pred <- loo_serial(d, encoding$fnames, fit_and_predict_fn)
   }
   correct <- pred == as.character(d$y)
-  result <- data_frame(avg_loo=mean(correct),
-                       predictions=list(data_frame(label=d$label,
+  result <- dplyr::data_frame(avg_loo=mean(correct),
+                       predictions=list(dplyr::data_frame(label=d$label,
                                                    y=d$y,
                                                    pred=pred,
                                                    correct=correct)))
